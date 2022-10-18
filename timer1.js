2ef46541e3ddf51c99ab5e99f54857baeb51b2a6
JOON LEE <110060709+greenlemonkp@users.noreply.github.com>
2022-10-17 20:27:10 -0400

    simpleTimer2

--- timer1.js
+++ timer1.js
@@ -1,15 +1,12 @@
-let timer = function() {
-  let input = process.argv.splice(2);
+let input = process.argv.splice(2);
   for (let i of input) {
     if (Number(i) < 0 || Number(isNaN(i))) {
       continue;
     } else {
       setTimeout(() => {
-        process.stdout.write('\x07');
+        console.log("beep");
       }, Number(i) * 1000);
       i++;
     }
   }
-};
 
-timer();
